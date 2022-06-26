7830aca38ec4a6588ff62fd26ab6e9e3c3625d5e
matr1x244 <zio-21@yandex.ru>
2022-06-26 20:28:14 +0300

    Добавил -> KotlinDSL.buildSrc.main -> Dependencies.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(Dependencies.ANDROID_MATERIAL_DEPENDENCIES)
+    implementation(Dependencies.CONSTRAINT_LAYOUT_DEPENDENCIES)
 
 }
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,9 @@
+object Dependencies {
+
+   private const val ANDROID_MATERIAL = "1.6.1"
+   const val ANDROID_MATERIAL_DEPENDENCIES = "com.google.android.material:material:$ANDROID_MATERIAL"
+
+   private const val CONSTRAINT_LAYOUT = "2.1.4"
+   const val CONSTRAINT_LAYOUT_DEPENDENCIES = "androidx.constraintlayout:constraintlayout:$CONSTRAINT_LAYOUT"
+
+ }
